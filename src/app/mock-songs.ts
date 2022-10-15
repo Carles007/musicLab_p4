051e0f04f0c0ad95c80feaa5ef0ca63faa094392
spozomeza <spozomeza@uoc.edu>
2022-10-15 14:51:46 +0200

    Se agrega listado inicial de canciones

--- src/app/mock-songs.ts
+++ src/app/mock-songs.ts
@@ -2,7 +2,14 @@ import { Song } from "./song";
 
 export const SONGS: Song[] = [
 
-  { id: 14, name: 'name3', artist:'artist3', album: 'album3', year: 1999, time: '5:46', imagen:'path_imagen'},
-
+  { id: 14, name: 'Seek & Destroy', artist:'Metallica', album: 'Kill \'Em All', year: 1983, time: '6:54', imagen:'assets/img/metallica.png'},
+  { id: 1, name: 'The Trooper', artist:'Iron Maiden', album: 'Piece of Mind', year: 1983, time: '4:12', imagen:'assets/img/ironmaiden.png'},
+  { id: 2, name: 'name3', artist:'artist3', album: 'album3', year: 1999, time: '5:46', imagen:'assets/img/metallica.png'},
+  { id: 3, name: 'name4', artist:'artist4', album: 'album4', year: 1999, time: '5:46', imagen:'assets/img/metallica.png'},
+  { id: 4, name: 'name5', artist:'artist5', album: 'album5', year: 1999, time: '5:46', imagen:'assets/img/metallica.png'},
+  { id: 5, name: 'name6', artist:'artist6', album: 'album6', year: 1999, time: '5:46', imagen:'assets/img/metallica.png'},
+  { id: 6, name: 'name7', artist:'artist7', album: 'album7', year: 1999, time: '5:46', imagen:'assets/img/metallica.png'},
+  { id: 7, name: 'name8', artist:'artist8', album: 'album8', year: 1999, time: '5:46', imagen:'assets/img/metallica.png'},
+  { id: 8, name: 'name9', artist:'artist9', album: 'album9', year: 1999, time: '5:46', imagen:'assets/img/metallica.png'}
 
 ]
