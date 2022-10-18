ae35685bad95de5c158512afa1d096f1c732fffb
JacobRC <jakomania@gmail.com>
2022-10-18 15:28:46 +0200

    Añadido contenido para el reproductor

--- src/app/mock-songs.ts
+++ src/app/mock-songs.ts
@@ -4,12 +4,12 @@ export const SONGS: Song[] = [
 
   { id: 14, name: 'Seek & Destroy', artist:'Metallica', album: 'Kill \'Em All', year: 1983, time: '6:54', imagen:'assets/img/metallica.png'},
   { id: 1, name: 'The Trooper', artist:'Iron Maiden', album: 'Piece of Mind', year: 1983, time: '4:12', imagen:'assets/img/ironmaiden.png'},
-  { id: 2, name: 'name3', artist:'artist3', album: 'album3', year: 1999, time: '5:46', imagen:'assets/img/metallica.png'},
-  { id: 3, name: 'name4', artist:'artist4', album: 'album4', year: 1999, time: '5:46', imagen:'assets/img/metallica.png'},
-  { id: 4, name: 'name5', artist:'artist5', album: 'album5', year: 1999, time: '5:46', imagen:'assets/img/metallica.png'},
-  { id: 5, name: 'name6', artist:'artist6', album: 'album6', year: 1999, time: '5:46', imagen:'assets/img/metallica.png'},
-  { id: 6, name: 'name7', artist:'artist7', album: 'album7', year: 1999, time: '5:46', imagen:'assets/img/metallica.png'},
-  { id: 7, name: 'name8', artist:'artist8', album: 'album8', year: 1999, time: '5:46', imagen:'assets/img/metallica.png'},
-  { id: 8, name: 'name9', artist:'artist9', album: 'album9', year: 1999, time: '5:46', imagen:'assets/img/metallica.png'}
+  { id: 2, name: 'The Fat Man', artist:'Fats Domino', album: 'Fats Domino Swings', year: 1959, time: '2:43', imagen:'assets/img/fatsdomino.jpg'},
+  { id: 3, name: 'Breathe', artist:'Pink Floyd', album: 'Dark Side of the Moon', year: 1973, time: '2:49', imagen:'assets/img/pink-floyd.jpg'},
+  { id: 4, name: 'Show Must Go On', artist:'Queen', album: 'Innuendo', year: 1991, time: '4:37', imagen:'assets/img/Queen.jpg'},
+  { id: 5, name: 'Black Dog', artist:'Led Zeppelin', album: 'Led Zeppelin', year: 1971, time: '4:55', imagen:'assets/img/ledzeppelin.jpeg'},
+  { id: 6, name: 'Purple Haze', artist:'Jimi Hendrix', album: 'Are You Experienced', year: 1967, time: '2:50', imagen:'assets/img/hendrix.jpg'},
+  { id: 7, name: 'You Dont Know Me', artist:'Ray Charles', album: 'Modern Sounds in Country and Western Music', year: 2009, time: '3:16', imagen:'assets/img/ray.jpg'},
+  { id: 8, name: 'Kiss', artist:'Prince', album: 'Parade', year: 1986, time: '3:46', imagen:'assets/img/prince.png'}
 
 ]
