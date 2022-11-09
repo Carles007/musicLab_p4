071d18899341fd6b2385e5f42285f5f4c74e4180
Carles007 <76649288+Carles007@users.noreply.github.com>
2022-11-09 21:03:27 +0100

    boton sonido subir/bajar, doble columna, formato botones play..

--- src/app/music-control/music-control.component.ts
+++ src/app/music-control/music-control.component.ts
@@ -11,6 +11,7 @@ export class MusicControlComponent implements OnInit {
 
   audio: HTMLAudioElement = new Audio();
   playing: boolean = false;
+  showVolume: boolean = false;
  // volume: number=0.5;
 
   constructor() { }
