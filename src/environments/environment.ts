f8033166c2ad02f3cdce276266e0c06787672d27
pmunozge <pmunozge@uoc.edu>
2022-12-10 17:54:55 +0100

    envirmoent

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // Your web app's Firebase configuration
+  firebase : {
+  apiKey: "AIzaSyBmgPh8Pl-CIBARwUBFa_Mw9a4IKYt6N6M",
+  authDomain: "p4hybrid-b3094.firebaseapp.com",
+  projectId: "p4hybrid-b3094",
+  storageBucket: "p4hybrid-b3094.appspot.com",
+  messagingSenderId: "352910118356",
+  appId: "1:352910118356:web:ec8d2670957a2a38111a41"
+}
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
